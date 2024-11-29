70822f8613a0211611b3323c6f422e770af635da
Sarah <23024100@myrp.edu.sg>
2024-11-29 15:54:33 +0800

    COMMIT

--- travel-list-app-starter-main/src/components/Stats.js
+++
@@ -1,7 +0,0 @@
-export default function Stats() {
-    return (
-      <footer className="stats">
-        <em>You have X items in the list. You already packed Y (Z%).</em>
-      </footer>
-    );
-  }
